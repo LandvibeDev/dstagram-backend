87b0d403678b3db16d11af60e5c3392932866ea0
Doyeong An <ady404@gmail.com>
2020-08-01 00:32:22 +0900

    fix

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-input=$1
-if [ $input = "-l" ]
-then
-    echo "local build"
-else
-    cp application.yml ~/dstagram-backend/src/main/resources/application.yml
-fi
+cp application.yml ~/dstagram-backend/src/main/resources/application.yml
 cd ~/dstagram-backend
 ./gradlew build -x test
 cp build/libs/*.war docker/war/
